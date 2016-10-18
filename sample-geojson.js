b40a91c839c8239ba00311af7b4c02bcbfdaf9c5
ES1 Small <es1@example.com>
2016-10-18 11:54:54 +0200

    Remove unwanted freebus

--- sample-geojson.js
+++ sample-geojson.js
@@ -6,8 +6,8 @@ var freeBus = {
             "geometry": {
                 "type": "LineString",
                 "coordinates": [
-                    [-105.00341892242432, 39.75383843460583],
-                    [-105.0008225440979, 39.751891803969535]
+                    [8.6833, 50.116],
+                    [8.683, 50.117]
                 ]
             },
             "properties": {
@@ -15,36 +15,6 @@ var freeBus = {
                 "underConstruction": false
             },
             "id": 1
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "LineString",
-                "coordinates": [
-                    [-105.0008225440979, 39.751891803969535],
-                    [-104.99820470809937, 39.74979664004068]
-                ]
-            },
-            "properties": {
-                "popupContent": "This is free bus that will take you across downtown.",
-                "underConstruction": true
-            },
-            "id": 2
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "LineString",
-                "coordinates": [
-                    [-104.99820470809937, 39.74979664004068],
-                    [-104.98689651489258, 39.741052354709055]
-                ]
-            },
-            "properties": {
-                "popupContent": "This is free bus that will take you across downtown.",
-                "underConstruction": false
-            },
-            "id": 3
         }
     ]
 };
